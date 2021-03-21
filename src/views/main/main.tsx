b02088e2ce486006d5c102158ee15ec60d68c6c2
Christian Freitas <chrfreitas@gmail.com>
2021-03-21 15:38:30 -0300

    Change main view

--- src/views/main/main.tsx
+++ src/views/main/main.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { Container } from './main.styles';
 
 function Main() {
-  return <Container>Chris</Container>;
+  return (
+    <Container>
+      Chris
+      <small>Freitas</small>
+    </Container>
+  );
 }
 
 export default Main;
